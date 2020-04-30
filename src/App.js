2fe00ecae9e112b37fa4acb99ef37dc3fa55708f
Senthurapandi1607 <senthurapandi.sv.2016.cse@rajalakshmi.edu.in>
2020-04-30 11:09:07 +0530

    29 April Commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 //import forms from "./component/Forms";
-import Form from "./Form";
+//import Form from "./Form";
+import First from "./First";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      
+      <First />
     </div>
   );
 }
